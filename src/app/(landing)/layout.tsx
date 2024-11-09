763ebfb568ef5e62bf8a7a1cbafbaab266b579dc
yoshikazuuu <febrianojerry@gmail.com>
2024-11-09 21:15:39 +0700

    feat: header

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 
 import Footer from "@/components/layout/footer";
+import Header from "@/components/layout/header";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
+      <Header />
       {children}
       <Footer />
     </>
