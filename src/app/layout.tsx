e88dd6fbd82dbfb5604db0f41e444d5d1da8b078
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-11-10 14:58:41 +0700

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,27 @@ import { TRPCReactProvider } from "@/trpc/react";
 export const metadata: Metadata = {
   title: "TransitPlus",
   description: "Your Journey to a Sustainable Future Starts Here!",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  openGraph: {
+    url: "https://transitplus.rey.mba",
+    siteName: "TransitPlus",
+    images: [
+      {
+        url: "https://img.jer.ee/img/transitplus-1511eee0.png",
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: "https://img.jer.ee/img/transitplus-1511eee0.png",
+        width: 1200,
+        height: 630,
+        alt: "og",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  icons:
+    "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚃</text></svg>",
 };
 
 const inter = Inter({
