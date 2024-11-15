d7b70469c40b920014906e62a784f60a9de08630
reynaldomarchell <reynaldomarchellba@gmail.com>
2024-11-15 22:19:30 +0700

    fix: flip words

--- src/app/(landing)/components/random-text.tsx
+++ src/app/(landing)/components/random-text.tsx
@@ -3,12 +3,16 @@
 import { FlipWords } from "@/components/ui/flip-words";
 
 export function RandomText() {
-  const arrayOfText = ["Ride", "Earn", "Thrive"];
+  const arrayOfText = [
+    "Take a ride with us",
+    "Earn while you drive",
+    "Thrive with our community",
+  ];
 
   return (
     <FlipWords
       words={arrayOfText}
-      duration={1500}
+      duration={3000}
       className="p-0 text-4xl font-extrabold tracking-tighter text-background sm:text-5xl md:text-6xl lg:text-7xl"
     />
   );
